8e900ba367057f784438daa5f3f37ab68bc6d3eb
Martin smith <msmith3dart@icloud.com>
2018-12-04 16:27:12 -0800

    add solutions for 7 & 8

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def islower(c):
+    var = ord(c)
+    if var < 97:
+        return False
+    return True
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    ls = list(str);
+    for i in range(len(str)):
+        char = ord(ls[i])
+        if  char >= ord('a') and char <= ord('z'):
+            ls[i] = chr(char - 32)
+    print("".join(ls))
