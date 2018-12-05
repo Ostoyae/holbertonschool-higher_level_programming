93500c4b95f1c21babdb44e33a046290b959e592
Martin smith <msmith3dart@icloud.com>
2018-12-04 16:39:44 -0800

    pep8 style fixes

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for a in range(0, 9):
     for b in range(a + 1, 10):
         if a < 8:
-            print("{}{},".format(a,b), end = " ")
+            print("{}{},".format(a, b), end=" ")
         else:
-            print("{}{}".format(a,b))
+            print("{}{}".format(a, b))
