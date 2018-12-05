859dfadf8eea450e59849a0fd1b1bb3eb3deff79
Martin Smith <538@holbertonschool.com>
2018-12-05 23:20:06 +0000

    hotfix 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     lnth = len(sys.argv) - 1
     print(
-            "{} arguments".format(lnth),
+            "{} argument{}".format(lnth, 's' if not lnth or lnth > 1 else ''),
             end="{}".format(".\n") if not lnth else ":\n")
     if lnth > 0:
         for idx, item in enumerate(sys.argv):
