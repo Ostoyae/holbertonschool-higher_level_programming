1e59e226b9f1d28c0773606136260a60fb4810e7
Martin smith <msmith3dart@icloud.com>
2018-12-04 20:31:53 -0800

    hotfix task 13
    If head is Null now will create a new node

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -15,7 +15,7 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *temp;
 
 	if (!head || !(*head))
-		return (*head = new_node(number,NULL));
+		return (*head = new_node(number, NULL));
 	temp = *head;
 
 	while (temp)
