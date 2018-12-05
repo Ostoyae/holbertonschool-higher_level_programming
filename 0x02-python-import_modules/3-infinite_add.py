f863750885fcaddd66b94d58d518572e6e0342df
Martin smith <msmith3dart@icloud.com>
2018-12-05 12:44:32 -0800

    modify task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+    sum = 0
     lnth = len(sys.argv) - 1
     if not lnth:
         print("0")
-        return
     else:
-        for i in sys.argv:
-            sum +=
-    
+        for idx, i in enumerate(sys.argv):
+            if idx != 0:
+                sum += int(i)
+    if sum:
+        print(sum)
