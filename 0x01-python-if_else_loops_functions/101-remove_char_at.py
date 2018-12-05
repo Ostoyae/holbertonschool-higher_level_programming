8bd5e827559bda98ccebac4ec54f0d5253e1d6da
Martin smith <msmith3dart@icloud.com>
2018-12-04 21:33:49 -0800

    hotfix task 15
    remove print() and return str

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    if len(str) < n:
-        print(str)
-        return
+    if len(str) < n or n < 0:
+        return str
     ls = []
     for i, c in enumerate(str):
         if i != n:
