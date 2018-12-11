2b15559c00f36a9a9165c3e9a7d09f24aa7038de
Martin smith <msmith3dart@icloud.com>
2018-12-11 15:57:13 -0800

    clean up

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or len(my_list) - 1 < idx:
         return my_list
-    for i , ele in enumerate(my_list):
+    for i, ele in enumerate(my_list):
         if idx == i:
             del my_list[i]
     return my_list
