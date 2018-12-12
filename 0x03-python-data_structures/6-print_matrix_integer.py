9b128c170643f4e994961f279f877039ced741b9
Martin smith <msmith3dart@icloud.com>
2018-12-11 16:33:37 -0800

    update 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,11 +2,12 @@
 def print_matrix_integer(matrix=[[]]):
     if type(matrix) == list:
         for pos in matrix:
-            lngth = len(pos)
-            for i in range(lngth):
-                print(
-                        "{:d}".format(pos[i]),
-                        end="{}".format(
-                            " " if i < lngth - 1 else "\n"))
-    
-    return None
+            if type(pos) == list:
+                lngth = len(pos)
+                for i in range(lngth):
+                    print(
+                            "{:d}".format(pos[i]),
+                            end="{}".format(
+                                " " if i < lngth - 1 else "\n"))
+            else:
+                return None
