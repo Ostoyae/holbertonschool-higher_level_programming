1d07dc9bffc131cd70a51fb234a79b98fc4b5811
Martin smith <msmith3dart@icloud.com>
2018-12-11 18:33:48 -0800

    update 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,14 +2,28 @@
 
 int rec_pal(listint_t **head, listint_t *tail);
 
+/**
+ * is_palindrome - check if a link list is a palindrome
+ * @head: double pointer to head
+ *
+ * Return: 1 if palindrome else 0
+ */
 int is_palindrome(listint_t **head)
 {
 	return (rec_pal(head, (*head)->next));
 }
 
+/**
+ * rec_pal - recusivly go through a link like to determin if it's an palindrome
+ * @head: double pointer to head
+ * @tail: pointer to node in a link list
+ *
+ * Return: 1 if palindrome else 0
+ */
 int rec_pal(listint_t **head, listint_t *tail)
 {
 	int status = 0;
+
 	if (tail)
 	{
 		status = rec_pal(head, tail->next);
