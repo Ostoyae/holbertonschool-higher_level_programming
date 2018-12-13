2379d0932061273847afc97059a3d12e8de21bfe
Martin Smith <msmith3dart@icloud.com>
2018-12-13 21:48:55 +0000

    fix style 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     board = a_dictionary
     player = None
     score = 0
-    if type(a_dictionary) == dict or a_dictionary != None:
+    if type(a_dictionary) == dict or a_dictionary is not None:
         for k in a_dictionary:
             if board.get(k) > score:
                 score = board.get(k)
