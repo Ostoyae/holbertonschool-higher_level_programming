636011ca1c1145898a400cafaa1d149a6e660de7
Martin Smith <msmith3dart@icloud.com>
2018-12-13 21:42:58 +0000

    add solution to 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    board = a_dictionary
+    player = None
+    score = 0
+    if type(a_dictionary) == dict or a_dictionary != None:
+        for k in a_dictionary:
+            if board.get(k) > score:
+                score = board.get(k)
+                player = k
+
+    return player
