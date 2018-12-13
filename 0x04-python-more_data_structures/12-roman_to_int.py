4deb70d3ce9c04b60c66b55866c9e6716c03a14e
Martin Smith <msmith3dart@icloud.com>
2018-12-13 23:15:22 +0000

    update 12
    process lowercase

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,16 +6,13 @@ def roman_to_int(roman_string):
             'I': 1, 'V': 5, 'X': 10,
             'L': 50, 'C': 100, 'D': 500,
             'M': 1000}
-    ls = list(roman_string)
-    s = set(roman_string)
+    ls = list(roman_string.upper())
     num = 0
 
     for n in ls:
         if n in list(rome):
             if num < 4000:
                 num += rome.get(n)
-            else:
-                return 0
         else:
             continue
 
