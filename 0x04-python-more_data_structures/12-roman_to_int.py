28314007deeceea4e1a93e82f24b4d3bce716383
Martin Smith <msmith3dart@icloud.com>
2018-12-14 02:03:18 +0000

    recode task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,13 +5,26 @@ def roman_to_int(roman_string):
     rome = {
             'I': 1, 'V': 5, 'X': 10,
             'L': 50, 'C': 100, 'D': 500,
-            'M': 1000}
-    ls = list(roman_string.upper())
+            'M': 1000, 'IV': 4, 'IX': 10,
+            'XL': 40, 'XC': 90, 'CD': 400,
+            'CM': 900}
+    ls = list(reversed(roman_string.upper()))
+    pair = ""
     num = 0
-
-    for n in ls:
+    for i, n in enumerate(ls):
         if n in list(rome):
-            if num < 4000:
+            if i - 1 >= 0 :
+                if ls[i - 1] == 'V' and num <= 4:
+                    num -= 1
+                elif ls[i - 1] == 'X' and num <= 10:
+                    num -= 1 
+                elif (ls[i - 1] == 'L' or ls[i - 1] == 'C') and num <= 40:
+                    num +=  10
+                elif (ls[i - 1] == 'D' or ls[i - 1] == 'M') and num <= 400:
+                    num += 100
+                else:
+                    num += rome.get(n)
+            else:
                 num += rome.get(n)
         else:
             continue
