52d9f7c4bdae506fa3fd5e75abf0c8047b73e7cd
Martin Smith <msmith3dart@icloud.com>
2019-01-02 21:06:17 +0000

    test is list is empty

--- 0x05-python-exceptions/test/0-main.py
+++ 0x05-python-exceptions/test/0-main.py
@@ -10,3 +10,7 @@ print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list(my_list, len(my_list) + 2)
 print("nb_print: {:d}".format(nb_print))
 
+my_list = []
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+
