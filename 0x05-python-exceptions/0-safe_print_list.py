4e4db23ae1753dfa090929eea54faff0d94fb462
Martin Smith <msmith3dart@icloud.com>
2019-01-02 21:05:52 +0000

    pep8 corrections

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,10 @@
-#/usr/bin/python3
-
+#!/usr/bin/python3
 def safe_print_list(mylist=[], x=0):
     i = 0
     while i < x:
         try:
             print("{}".format(mylist[i]), end="")
-            i += 1 
+            i += 1
         except IndexError:
             break
     print()
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def safe_print_division(a,b):
+def safe_print_division(a, b):
     sum = None
     try:
         sum = a / b
