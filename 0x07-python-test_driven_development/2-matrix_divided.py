bc044f9e9ccd87eaea9c7d6995b528b5be606814
Martin Smith <msmith3dart@icloud.com>
2019-01-09 17:57:49 +0000

    fix error format

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -18,34 +18,33 @@ def matrix_divided(matrix, div):
     all values divided by div will be rounded to the 2 decimal place
     """
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of\
-                        integers/floats")
+        raise_valueType()
     try:
         if not isinstance(matrix[0], list):
-            raise TypeError("matrix must be a matrix (list of lists) of\
-                             integers/floats")
+            raise_valueType()
         size = len(matrix[0])
         if size is 0:
-            raise TypeError("matrix must be a matrix (list of lists) of\
-                            integers/floats")
+            raise_valueType()
     except IndexError:
-        raise TypeError("matrix must be a matrix (list of lists) of\
-            integers/floats")
+        raise_valueType()
     if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
     for row in matrix:
         if len(row) is not size:
-            raise TypeError("Each row of the matrix must have the\
-                            same size")
+            raise TypeError("Each row of the matrix must have the same size")
     try:
         mtx_cpy = list(map(lambda row: list(
             map(lambda ele: round(ele / div, 2), row)), matrix))
     except (ValueError, TypeError):
-        raise TypeError("matrix must be a matrix (list of lists) of\
-                        integers/floats")
+        raise_valueType()
     except ZeroDivisionError:
         raise ZeroDivisionError("division by zero")
     return mtx_cpy
+
+
+def raise_valueType():
+    err = "matrix must be a matrix (list of lists) of integers/floats"
+    raise TypeError(err)
 
 
 if __name__ == "__main__":
