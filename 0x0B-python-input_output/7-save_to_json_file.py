a762fbcbf30161a99feca435a767943d53d997f6
Martin Smith <538@holbertonschool.com>
2019-01-17 21:29:08 +0000

    shorten code

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -5,6 +5,5 @@ def save_to_json_file(my_obj, filename):
     """function that writes an Object to a text file, using a JSON
     representation
     """
-    j_obj = json.JSONEncoder().encode(my_obj)
     with open(filename, mode='w', encoding='utf-8') as f:
-        f.write(j_obj)
+        f.write(json.JSONEncoder().encode(my_obj))
