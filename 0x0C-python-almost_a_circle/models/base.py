8d9bbff2b5d2e8ff8f41089402501a4984304eae
Martin Smith <msmith3dart@icloud.com>
2019-01-23 08:08:29 +0000

    Add methods
    ADD:\n * to_json_string()\n * save_to_file()

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import json
 
 class Base():
     """ Base class for modules base.
@@ -8,8 +9,32 @@ class Base():
 
 
     def __init__(self, id=None):
+        """ Creates an ID"""
         if id:
             self.id = id
         else:
             self.__nb_objects += 1
             self.id = self.__nb_objects
+    
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ convert object/dic into json strings
+        """
+        dictionary = []
+        if not list_dictionaries:
+            return []
+        for ele in list_dictionaries:
+            if isinstance(ele, dict):
+                dictionary.append(ele)
+            else:
+                dictionary.append(ele.to_dictionary())
+
+        return json.JSONEncoder().encode(dictionary)
+
+    @classmethod
+    def save_to_file(cls, list_objc):
+        """ save a json file"""
+        with open("{}.json".format(cls.__name__), "w") as f:
+            json_str = cls.to_json_string(list_objc)
+            f.write(json_str)
+        
