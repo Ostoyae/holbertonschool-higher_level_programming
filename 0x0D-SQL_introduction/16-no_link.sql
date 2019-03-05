86f0137d543bd56fe8203a626891b4dbf2d3ace1
Martin Smith <msmith3dart@icloud.com>
2019-03-05 06:01:07 +0000

    update

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,4 @@
 -- List all records of table `second_table` with score and name in Desc order
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table
+WHERE name IS NOT NULL
+ORDER BY score DESC;
