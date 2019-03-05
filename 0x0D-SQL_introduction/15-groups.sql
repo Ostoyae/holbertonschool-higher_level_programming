1f8df0bbc8b2eb6052ba10d899a8628edd1c03dd
Martin Smith <msmith3dart@icloud.com>
2019-03-05 05:52:48 +0000

    hotfix: count field name changed to number

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- list all the records with the same score and the count.
-SELECT score, COUNT(*) FROM second_table GROUP BY score DESC;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score DESC;
