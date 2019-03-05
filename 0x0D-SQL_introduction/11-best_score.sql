f8e970abc0c8907c91315da610baa96005d55e07
Martin Smith <msmith3dart@icloud.com>
2019-03-05 05:50:45 +0000

    correct output

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- list two best the records of second_stable ordered from highest to lowest score.
-SELECT score, name FROM second_table ORDER BY score DESC LIMIT 2;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
