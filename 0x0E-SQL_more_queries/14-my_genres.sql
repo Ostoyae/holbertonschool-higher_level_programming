cb5ebdecc71926119344dca76cf06cd1ee52080a
Martin Smith <msmith3dart@icloud.com>
2019-03-06 05:29:55 +0000

    fix order

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,4 +4,5 @@ FROM tv_genres
 LEFT JOIN (tv_show_genres CROSS JOIN tv_shows)
 ON (tv_genres.id = tv_show_genres.genre_id
 AND tv_show_genres.show_id = tv_shows.id)
-WHERE tv_shows.title = 'Dexter';
+WHERE tv_shows.title = 'Dexter'
+ORDER BY name ASC;
