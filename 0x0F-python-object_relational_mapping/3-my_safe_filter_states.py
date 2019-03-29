31a113b3909fd40c024f3e96bb566b7fa17abfe4
Martin Smith <msmith3dart@icloud.com>
2019-03-29 19:15:52 +0000

    hotfix

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,30 +4,32 @@
 import sys
 import MySQLdb
 
-args = sys.argv
-db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=args[1],
-        password=args[2],
-        db=args[3]
-        )
-
-cur = db.cursor()
-
-try:
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id"
-    cur.execute(query, (args[4],))
-    rows = cur.fetchall()
-except MySQLdb.error as e:
-    try:
-        print("MySQL Error [{:d}]: {:s}".format(e.args[0], e.args[1]))
-    except IndexError:
-        print("MySQL Erro: {}".format(e))
-for row in rows:
-    print("({}, {})".format(row[0], row[1]))
-cur.close()
-db.close()
-
 if __name__ == "__main__":
-    pass
+    args = sys.argv
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=args[1],
+            password=args[2],
+            db=args[3]
+            )
+
+    cur = db.cursor()
+
+    try:
+        query = '''
+        SELECT *
+        FROM states
+        WHERE name = %s
+        ORDER BY id ASC'''
+        cur.execute(query, (args[4],))
+        rows = cur.fetchall()
+    except MySQLdb.error as e:
+        try:
+            print("MySQL Error [{:d}]: {:s}".format(e.args[0], e.args[1]))
+        except IndexError:
+            print("MySQL Erro: {}".format(e))
+    for row in rows:
+        print("({}, '{}')".format(row[0], row[1]))
+    cur.close()
+    db.close()
