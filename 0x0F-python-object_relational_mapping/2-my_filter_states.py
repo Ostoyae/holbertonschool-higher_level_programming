6ff7291336565b062c8e569d85009721861a062c
Martin Smith <msmith3dart@icloud.com>
2019-03-29 19:04:07 +0000

    update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,31 +3,33 @@ import sys
 import MySQLdb
 """Filter for state
 """
-args = sys.argv
-db = MySQLdb.connect(
+if __name__ == "__main__":
+    args = sys.argv
+    db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=args[1],
         password=args[2],
         db=args[3]
-        )
+    )
 
-cur = db.cursor()
+    cur = db.cursor()
 
-try:
-    query = "SELECT * FROM states WHERE name = '{:s}' ORDER BY id".format(args[4])
-    cur.execute(query)
-    rows = cur.fetchall()
-except MySQLdb.error as e:
     try:
-        print("MySQL Error [{:d}]: {:s}".format(e.args[0], e.args[1]))
-    except IndexError:
-        print("MySQL Erro: {}".format(e))
+        query = '''
+        SELECT *
+        FROM states
+        WHERE name = '{:s}'
+        ORDER BY id ASC'''.format(args[4])
+        cur.execute(query)
+        rows = cur.fetchall()
+    except MySQLdb.error as e:
+        try:
+            print("MySQL Error [{:d}]: {:s}".format(e.args[0], e.args[1]))
+        except IndexError:
+            print("MySQL Erro: {}".format(e))
 
-for row in rows:
-    print("({}, {})".format(row[0], row[1]))
-cur.close()
-db.close()
-
-if __name__ == "__main__":
-    pass
+    for row in rows:
+        print("({}, {})".format(row[0], row[1]))
+    cur.close()
+    db.close()
