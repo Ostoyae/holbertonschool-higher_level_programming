fd4115517c0865b8bc11a6d792321dc48be9b805
Martin Smith <msmith3dart@icloud.com>
2019-03-29 18:25:25 +0000

    hotfix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,34 +1,35 @@
 #!/usr/bin/env python3
 """Filter states in a table that start with "N" and ordered by `id`
 """
-
 import sys
 import MySQLdb
 
-args = sys.argv
-db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=args[1],
-        password=args[2],
-        db=args[3]
-        )
-
-cur = db.cursor()
-
-try:
-    cur.execute('SELECT * FROM states WHERE `name` LIKE "N%" ORDER BY `id`')
-    rows = cur.fetchall()
-except MySQLdb.error as e:
-    try:
-        print("MySQL Error [{:d}]: {:s}".format(e.args[0], e.args[1]))
-    except IndexError:
-        print("MySQL Erro: {}".format(e))
-
-for row in rows:
-    print("({}, {})".format(row[0], row[1]))
-cur.close()
-db.close()
-
 if __name__ == "__main__":
-    pass
+    args = sys.argv
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=args[1],
+            password=args[2],
+            db=args[3]
+            )
+
+    cur = db.cursor()
+
+    try:
+        cur.execute('''
+        SELECT * 
+        FROM states 
+        WHERE `name` LIKE "N%"
+        ORDER BY `id` ASC''')
+        rows = cur.fetchall()
+    except MySQLdb.error as e:
+        try:
+            print("MySQL Error [{:d}]: {:s}".format(e.args[0], e.args[1]))
+        except IndexError:
+            print("MySQL Erro: {}".format(e))
+
+    for row in rows:
+        print("({}, '{}')".format(row[0], row[1]))
+    cur.close()
+    db.close()
