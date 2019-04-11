9043adb43b9d52abb9b0d77aed78c6fe4e1f8268
Martin Smith <msmith3dart@icloud.com>
2019-04-11 05:05:59 +0000

    update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # used curl to get the byte size of the content.
-curl -sI $1 | grep -i Content-Length | awk '{print $2}'
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
