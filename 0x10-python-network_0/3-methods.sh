90961371c0b4f49f300d290686330e6cd70036ba
Martin Smith <msmith3dart@icloud.com>
2019-04-11 06:18:57 +0000

    update

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a DELETE request to the URL
-curl -sfIL "$1" | grep -i Allow | awk '{$1 = ""; print $0}'
+curl -sfIL "$1" | grep -i Allow | awk '{$1 = ""; print $0}' | sed 's/^[ \s]//'
