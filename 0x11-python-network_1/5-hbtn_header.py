bf61bcddc9d4222c34616dbaae8cf757a6ce77c3
Martin Smith <msmith3dart@icloud.com>
2019-04-16 01:07:27 +0000

    upadte

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,6 +4,5 @@ if __name__ == "__main__":
     from sys import argv
     import requests
 
-    if len(argv) == 2:
-        res = requests.get(argv[1])
-        print(res.headers['X-Request-Id'])
+    res = requests.get(argv[1])
+    print(res.headers['X-Request-Id'])
