f4a1060eca008f40066b7d9e8d747537581aa38d
Martin Smith <msmith3dart@icloud.com>
2019-04-16 00:58:39 +0000

    add main check

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 # send a request to a url and also handle error codes
-from urllib import request, error
-import sys
+if __name__ == '__main__':
+    from urllib import request, error
+    import sys
 
-req = request.Request(sys.argv[1])
-try:
-    with request.urlopen(req) as responce:
-        print(responce.read().decode('utf-8'))
-except error.HTTPError as e:
-    print('Error code: {}'.format(e.code))
+    req = request.Request(sys.argv[1])
+    try:
+        with request.urlopen(req) as responce:
+            print(responce.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print('Error code: {}'.format(e.code))
