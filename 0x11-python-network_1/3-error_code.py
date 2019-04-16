e15b09f54606b3a84c5967a344d7a36207f7db2f
Martin Smith <msmith3dart@icloud.com>
2019-04-16 00:27:42 +0000

    update

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 # send a request to a url and also handle error codes
-from urllib import request
-from urllib.error import HTTPError
+import urllib
 from sys import argv, exit
 
-if len(argv) == 1:
-    exit()
-
 try:
-    with request.urlopen(argv[1]) as responce:
+    with urllib.request.urlopen(argv[1]) as responce:
         print(responce.read().decode('utf-8'))
-except HTTPError as e:
+except urllib.error.HTTPError as e:
     print('Error code: {}'.format(e.code))
