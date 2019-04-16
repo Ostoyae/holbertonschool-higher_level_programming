f04b4104f0ff53574e97e44cd477b4d08c8a57e6
Martin Smith <msmith3dart@icloud.com>
2019-04-16 01:05:38 +0000

    add __main__ check

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 # script will send a request to a URL return X-Request-Id
-from sys import argv
-import requests
+if __name__ == "__main__":
+    from sys import argv
+    import requests
 
-if len(argv) == 2:
-    res = requests.get(argv[1])
-    print(res.headers['X-Request-Id'])
+    if len(argv) == 2:
+        res = requests.get(argv[1])
+        print(res.headers['X-Request-Id'])
