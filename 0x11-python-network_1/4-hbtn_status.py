7563b7c2898cb89cb0e84bfdd8adefdb2d835a94
Martin Smith <msmith3dart@icloud.com>
2019-04-16 01:04:24 +0000

    add __main__ check

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 # This script fetches the body from https://intranet.hbtn.io/status
-import requests
+if __name__ == '__main__':
+    import requests
 
-response = requests.get('https://intranet.hbtn.io/status')
-print('Body response:')
-print("\t- type: {}".format(type(response.text)))
-print("\t- content: {}".format(response.text))
+    response = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
