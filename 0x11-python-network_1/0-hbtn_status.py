8b38b673c6b29be60bb3f2e027e87335ba1ec31e
Martin Smith <msmith3dart@icloud.com>
2019-04-16 01:04:34 +0000

    add __main__ check

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 # This script fetches the body from https://intranet.hbtn.io/status
-from urllib import request
+if __name__ == '__main__':
+    from urllib import request
 
-with request.urlopen('https://intranet.hbtn.io/status') as responce:
-    print('Body response:')
-    html = responce.read()
-    cont = html.decode('utf-8')
-    print("\t- type: {}".format(type(responce.read())))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(cont))
+    with request.urlopen('https://intranet.hbtn.io/status') as responce:
+        print('Body response:')
+        html = responce.read()
+        cont = html.decode('utf-8')
+        print("\t- type: {}".format(type(responce.read())))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(cont))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 # script will send a request to a URL return X-Request-Id
-from urllib import request
-from sys import argv
+if __name__ == '__main__':
+    from urllib import request
+    from sys import argv
 
-if len(argv) == 2:
-    with request.urlopen(argv[1]) as responce:
-        header = responce.info()
-        print(header['X-Request-Id'])
+    if len(argv) == 2:
+        with request.urlopen(argv[1]) as responce:
+            header = responce.info()
+            print(header['X-Request-Id'])
