ba9c35942e02da9d5ac167665da5b4be83e28f90
Martin Smith <msmith3dart@icloud.com>
2019-04-20 05:15:48 +0000

    add peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+this script defind a function to find a peak
+"""
+
+def find_peak(list_of_integers):
+    ls = list_of_integers
+    peak = None
+    if not ls:
+        return peak
+    cur = int(len(ls) / 2)
+    if ls[cur - 1] <= ls[cur] and ls[cur] >= ls[cur + 1]:
+        peak = ls[cur]
+    if not peak:
+        peak = find_peak(ls[cur:])
+    if not peak:
+        peak = find_peak(ls[0:(cur - 1)])
+
+    return peak
+    
+    
+if __name__ == "__main__":
+    pass
