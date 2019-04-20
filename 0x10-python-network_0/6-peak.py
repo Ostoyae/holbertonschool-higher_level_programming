0374e22eddb6682f471bd67fdb6665355df3fdac
Martin Smith <msmith3dart@icloud.com>
2019-04-20 05:25:33 +0000

    updated

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,6 +9,11 @@ def find_peak(list_of_integers):
     if not ls:
         return peak
     cur = int(len(ls) / 2)
+    if ls[0] > ls[1] and ls[0] > ls[-1]:
+        return ls[0]
+    elif ls[-1] > ls[-2]:
+        return ls[-1]
+
     if ls[cur - 1] <= ls[cur] and ls[cur] >= ls[cur + 1]:
         peak = ls[cur]
     if not peak:
