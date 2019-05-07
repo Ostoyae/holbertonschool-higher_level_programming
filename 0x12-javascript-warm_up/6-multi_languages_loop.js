d1f4c150d73e5bba9e51ff0a056f5f94e7a97d63
Martin Smith <msmith3dart@icloud.com>
2019-05-07 22:59:48 +0000

    remove whiteline at top

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-// Script print 3 kind messages about 3 programing languages
-
+// Script print 3 kind messages about 3 programing languages using a loop
 const lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 lang.forEach((val) => {
