de64152a997082497d999b581abbf4eb56727da1
Martin Smith <msmith3dart@icloud.com>
2019-05-08 04:40:25 +0000

    swap to a loop type rather then forEach

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Script print 3 kind messages about 3 programing languages using a loop
 const lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-
-lang.forEach((val) => {
-  console.log(`${val}`);
-});
+let i = 0;
+while (i < lang.length) {
+  console.log(lang[i++]);
+};
