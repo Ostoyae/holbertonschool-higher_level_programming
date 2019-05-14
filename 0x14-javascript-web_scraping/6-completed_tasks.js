a546a664cda0cb8f0857c2f0937be06d7b0348a8
Martin Smith <msmith3dart@icloud.com>
2019-05-14 22:48:45 +0000

    modify logic
    strip out users that have zero completed tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -23,6 +23,7 @@ if (input.length >= 1) {
         if (!users[id]) { users[id] = 0; }
         if (u.completed === true) { users[id] += 1; }
       });
+      Object.entries(users).map((k, v) => { if (v === 0) delete users.k; });
       console.log(users);
     });
 }
