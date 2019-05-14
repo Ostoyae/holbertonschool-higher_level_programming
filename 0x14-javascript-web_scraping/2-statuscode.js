2fa78ec1446ca9103ac3a1b2891c4b91981138f3
Martin Smith <msmith3dart@icloud.com>
2019-05-14 22:07:41 +0000

    code style fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,7 @@
 // makes a request to a endpoint and log the status code
 
 const request = require('request');
-const url = process.argv.slice(2)[0]
+const url = process.argv.slice(2)[0];
 request(url, (err, res, body) => {
-	err ? console.error(err) : console.log('code: %d', res.statusCode);
+  err ? console.error(err) : console.log('code: %d', res.statusCode);
 });
