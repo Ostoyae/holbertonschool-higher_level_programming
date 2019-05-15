c518ea3cb9de364b29e8a9322ac42dff3e349055
Martin Smith <msmith3dart@icloud.com>
2019-05-15 00:48:05 +0000

    learing

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,16 +5,21 @@ const request = require('request');
 const person = 'https://swapi.co/api/people/18/';
 const url = process.argv.slice(2)[0];
 let data = [];
-
+let status = 200;
 if (url) {
   request.get(url)
     .on('error', (err) => {
-      console.error(err);
+      console.err(err);
     })
+    .on('response', (resp) => { status = resp.statusCode; })
     .on('data', (chunk) => {
       data.push(chunk);
     })
     .on('end', () => {
+      if (status !== 200) {
+        console.log(0);
+        return;
+      }
       let films = JSON.parse(Buffer.concat(data).toString());
       let count = 0;
       films.results.forEach((mov) => {
